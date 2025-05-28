04aefcd1f15006b39ac31853759c1ce8e2fc1e49
Marlon Quilante <marlonquilante@gmail.com>
2025-05-27 21:17:02 -0300

    Implementado a classe "Amigo" e a validação de inputs

---
+++ ClubeDaLeitura/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,16 @@
+﻿namespace ClubeDaLeitura.Compartilhado
+{
+    internal class EntidadeBase
+    {
+        public int id;
+
+        public bool TelefoneValido(string telefone)
+        {
+            if (telefone.Length < 14 || telefone.Length > 15 || telefone[0] != '(' ||
+                telefone[3] != ')' || (!telefone.Substring(5, 10).Contains('-')) || telefone[4] != ' ')
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ ClubeDaLeitura/ModuloAmigo/Amigo.cs
@@ -0,0 +1,25 @@
+﻿using ClubeDaLeitura.Compartilhado;
+
+namespace ClubeDaLeitura.ModuloAmigo
+{
+    internal class Amigo : EntidadeBase
+    {
+        public string nome;
+        public string nomeResponsavel;
+        public string telefone;
+
+        public string Validacao()
+        {
+            string erros = "";
+
+            if (nome.Length < 2 || string.IsNullOrWhiteSpace(nome))
+                erros += "O nome do amigo precisa conter mais de 1 caractere!\n";
+            if (nomeResponsavel.Length < 2 || string.IsNullOrWhiteSpace(nome))
+                erros += "O nome do responsável precisa conter mais de 1 caractere!\n";
+            if (!TelefoneValido(telefone))
+                erros += "O telefone digitado não é válido!";
+
+            return erros;
+        }
+    }
+}
