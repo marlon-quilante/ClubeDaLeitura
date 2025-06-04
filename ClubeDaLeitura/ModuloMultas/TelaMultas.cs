b2ca8eb656d492d1feba54847b828d88d4984c5b
Marlon Quilante <marlonquilante@gmail.com>
2025-06-03 23:15:59 -0300

    Criados os métodos para visualizar e quitar as multas

--- ClubeDaLeitura/ModuloMultas/TelaMultas.cs
+++ ClubeDaLeitura/ModuloMultas/TelaMultas.cs
@@ -11,24 +11,45 @@ namespace ClubeDaLeitura.ModuloMultas
             this.repositorioMultas = repositorioMultas;
         }
 
-        public override void Deletar()
-        {
-            base.Deletar();
-        }
-
         public override int OpcaoDoMenu()
         {
-            throw new NotImplementedException();
+            Console.Clear();
+            Console.WriteLine("------------------------");
+            Console.WriteLine($"Controle de Multas");
+            Console.WriteLine("------------------------");
+
+            Console.WriteLine("\nSelecione uma opção...\n");
+            Console.WriteLine("1 - Visulizar");
+            Console.WriteLine("2 - Quitar");
+            Console.WriteLine();
+
+            return int.Parse(Console.ReadLine());
         }
 
         protected override void ApresentarCabecalhoTabela()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("{0,-5} | {1,-25} | {2,-25} | {3,-20} | {4,-10} | {5,-10}",
+                "ID", "Data de Empréstimo", "Data De Devolução", "Data Atual", "Valor", "Status");
         }
 
         protected override void ApresentarLinhaTabela(EntidadeBase registro)
         {
-            throw new NotImplementedException();
+            Multas multas = (Multas)registro;
+
+            Console.WriteLine("{0,-5} | {1,-25} | {2,-25} | {3,-20} | {4,-10}",
+                multas.id, multas.emprestimo.dataEmprestimo, multas.emprestimo.dataDevolucao, 
+                DateTime.Now.ToShortDateString(), multas.valor, multas.status);
+        }
+
+        public void Quitar()
+        {
+            int idMultas = ObterID();
+            Multas multas = (Multas)repositorioMultas.BuscarRegistroPorID(idMultas);
+
+            multas.status = "Quitada";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Multa quitada com sucesso!");
+            Console.ResetColor();
         }
 
         protected override EntidadeBase ObterDados()
@@ -38,7 +59,7 @@ namespace ClubeDaLeitura.ModuloMultas
 
         protected override bool TemRestricao(EntidadeBase registro)
         {
-            throw new NotImplementedException();
+            return false;
         }
     }
 }
