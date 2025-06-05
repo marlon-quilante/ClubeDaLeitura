4dc9644c7dcbaa4389ad34fdac0c1c50f56799d6
Marlon Quilante <marlonquilante@gmail.com>
2025-06-05 19:33:36 -0300

    Implementado a classe Reserva

---
+++ ClubeDaLeitura/ModuloReserva/Reserva.cs
@@ -0,0 +1,36 @@
+﻿using ClubeDaLeitura.Compartilhado;
+using ClubeDaLeitura.ModuloAmigo;
+using ClubeDaLeitura.ModuloRevista;
+
+namespace ClubeDaLeitura.ModuloReserva
+{
+    public class Reserva : EntidadeBase
+    {
+        public Amigo amigo;
+        public Revista revista;
+        public DateTime dataReserva = DateTime.Now;
+        public string status = "Ativa";
+
+        public Reserva(Amigo amigo, Revista revista, DateTime dataReserva)
+        {
+            this.amigo = amigo;
+            this.revista = revista;
+            this.dataReserva = dataReserva;
+        }
+
+        public override void Atualizar(EntidadeBase registroAtualizado)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RegistroExiste(EntidadeBase registro, RepositorioBase repositorio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Validacao(EntidadeBase registro, RepositorioBase repositorio)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
